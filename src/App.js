5304d0ef3253f4b9c8912f8e5dc2eda09f5622bf
Valentin Desneaux <vdesneaux@gmail.com>
2019-10-12 17:29:23 +0200

    Quest done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Contact
+      avatar="https://randomuser.me/api/portraits/women/29.jpg"
+      name = "Leta Cooper"
+      status
+    />
+
+    <Contact
+      avatar="https://randomuser.me/api/portraits/men/4.jpg"
+      name = "Dylan Peters"
+      
+    />
+    <Contact
+      avatar="https://randomuser.me/api/portraits/men/20.jpg"
+      name = "Darrell Parker"
+      status
+    />
+
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Contact.css';
+
+
+function Contact(props) {
+    return (
+      <div className="Contact">
+          <img src={props.avatar} alt={props.name} className="avatar" />
+          <div className="Info">
+              <h4>{props.name}</h4>
+              <div className="status">
+                 
+                 <p>
+                     <span className={props.status ? "status status-online":"status status-offline"}></span>{props.status ? "Online":"Offline"}
+                </p>
+                <hr/>
+              </div>
+              
+          </div>
+          
+      </div>
+      
+    );
+  }
+  
+  export default Contact;
