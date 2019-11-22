3a78c10fd53d331a5004e42399ea360333173b3e
Valentin Desneaux <vdesneaux@gmail.com>
2019-11-22 10:55:49 +0100

    quest react 03

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-    <Contact
-      avatar="https://randomuser.me/api/portraits/women/29.jpg"
-      name = "Leta Cooper"
-      status
-    />
-
-    <Contact
-      avatar="https://randomuser.me/api/portraits/men/4.jpg"
-      name = "Dylan Peters"
-      
-    />
-    <Contact
-      avatar="https://randomuser.me/api/portraits/men/20.jpg"
-      name = "Darrell Parker"
-      status
-    />
-
+    <ContactList />
     </div>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,16 +8,16 @@ function Contact(props) {
           <img src={props.avatar} alt={props.name} className="avatar" />
           <div className="Info">
               <h4>{props.name}</h4>
+              <span className="PhoneNumber">{props.phone}</span>
+              <p>{props.mail}</p>
               <div className="status">
-                 
                  <p>
                      <span className={props.status ? "status status-online":"status status-offline"}></span>{props.status ? "Online":"Offline"}
                 </p>
                 <hr/>
               </div>
-              
           </div>
-          
+        
       </div>
       
     );
